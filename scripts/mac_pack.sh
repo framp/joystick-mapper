961406841ed18bbf94490d6e988c4f2f6f2c8f74
Federico Rampazzo <frampone@gmail.com>
2020-12-09 02:21:16 +0000

    Bump version

--- scripts/mac_pack.sh
+++ scripts/mac_pack.sh
@@ -2,20 +2,20 @@
 echo -e "\n[profile.release]\nopt-level = 0" >> Cargo.toml
 cargo build --release 
 git checkout Cargo.toml
-rm joystick-mapper*.zip
-mkdir build
+rm -rf joystick-mapper*.zip
+mkdir -p build
 cp target/release/joystick-mapper build
 cp examples/sample.conf build/joystick-mapper.conf
 echo "Source code and instructions here: https://github.com/framp/joystick-mapper" > build/README.txt
 cd build
-zip joystick-mapper-mac-$1.zip build/* 
+zip joystick-mapper-mac-$1.zip * 
 cd ..
 mv build/*.zip . 
 rm build/joystick-mapper
 cp target/release/joystick-mapper-among-us build
 cp examples/among-us.conf build/joystick-mapper.conf
 cd build
-zip joystick-mapper-among-us-mac-$1.zip build/* 
+zip joystick-mapper-among-us-mac-$1.zip * 
 cd ..
 mv build/*.zip .
 rm -rf build
